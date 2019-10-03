e1151c4a2519f02a4ce10f497370e093fffc1c34
Salvador Medina Nava <developmentsmn@gmail.com>
2019-10-03 00:39:15 -0500

    new pictures

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+// Salvador
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
